8a8d398aead03a4746e98f78db8a611b8785ef1d
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-11-26 21:35:31 -0300

    Tests running

---
+++ test/jest-setup.ts
@@ -0,0 +1,9 @@
+import { SetupServer } from '@src/server';
+import supertest from 'supertest';
+
+beforeAll(async () => {
+  const server = new SetupServer();
+  server.init();
+  
+  global.testRequest = supertest(server.getApp());
+});
