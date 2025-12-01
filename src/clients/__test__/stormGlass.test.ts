0686dfcddadc206bf6d2df5752222bdbc62ca09f
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-11-30 21:42:55 -0300

    Create tests for Clients

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,1 +1,13 @@
-import axios, { AxiosStatic } from 'axios';
+import { StormGlass } from '../stormGlass';
+
+describe('StormGlass Client', () => {
+  it('should return normalized forecast data from the StormGlass service', async () => {
+    const lat = 58.7984;
+    const lng = 17.8081; 
+    
+    const stormGlass = new StormGlass();
+    const response = await stormGlass.fetchPoints(lat, lng);
+
+    expect(response).toEqual({});
+  });
+});
---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,6 @@
+export class StormGlass {
+  public async fetchPoints(lat: number, lng: number): Promise<{}> {
+    // Implementation to fetch data from StormGlass API would go here
+    return Promise.resolve({});
+  }
+}
