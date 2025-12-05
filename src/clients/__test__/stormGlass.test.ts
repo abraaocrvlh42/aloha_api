28c051650ae32a96714408ed49ceaafb5516d4b8
abraaocrvlh42 <abraaoncrvlh@gmail.com>
2025-12-05 20:13:32 -0300

    Create test for stormGlass and stormGlass clients

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,10 +1,15 @@
 import { StormGlass } from '../stormGlass';
+import axios from 'axios';
+
+jest.mock('axios');
 
 describe('StormGlass Client', () => {
   it('should return normalized forecast data from the StormGlass service', async () => {
     const lat = 58.7984;
     const lng = 17.8081; 
     
+    axios.get = jest.fn().mockResolvedValue({});
+
     const stormGlass = new StormGlass();
     const response = await stormGlass.fetchPoints(lat, lng);
 
